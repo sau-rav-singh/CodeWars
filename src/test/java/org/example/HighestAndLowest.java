68c1bb0a32971e86d4058368811acbcb26e40e1a
Saurav Singh <singh.saurav@icloud.com>
2023-10-12 08:26:35 +0530

    Initial Commit

---
+++ src/test/java/org/example/HighestAndLowest.java
@@ -0,0 +1,25 @@
+package kata;
+
+public class HighestAndLowest {
+
+	public static void main(String[] args) {
+		
+		System.out.println(highAndLow("1 2 -3 4 5"));// return "5 -3"
+	}
+
+	static String highAndLow(String numbers) {
+
+		String[] arr = numbers.split(" ");
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (Integer.parseInt(arr[i]) > max)
+				max = Integer.parseInt(arr[i]);
+			if (Integer.parseInt(arr[i]) < min)
+				min = Integer.parseInt(arr[i]);
+		}
+
+		return max + " " + min;
+	}
+}
---
+++ src/test/java/org/example/Mumbling.java
@@ -0,0 +1,27 @@
+package kata;
+
+public class Mumbling {
+
+	public static void main(String[] args) {
+//		accum("abcd") -> "A-Bb-Ccc-Dddd"
+//		accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//		accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+		System.out.println(accum("abcd"));
+	}
+
+	public static String accum(String s) {
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			result.append(Character.toUpperCase(c));
+			for (int j = 0; j < i; j++) {
+				result.append(Character.toLowerCase(c));
+			}
+			if (!(i == s.length()-1)) {
+				result.append('-');
+			}
+		}
+		return result.toString();
+	}
+}
