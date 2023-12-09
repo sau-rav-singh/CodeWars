95f4f57c75116cf8a4fc434cd8faedd81b538292
Saurav Singh <singh.saurav@icloud.com>
2023-12-09 11:07:13 +0530

    Added new Kata

--- src/test/java/FuntionalProgramming/FP01EX.java
+++ src/test/java/FuntionalProgramming/FP01EX.java
@@ -1,16 +1,22 @@
 package FuntionalProgramming;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FP01EX {
 
     public static void main(String[] args) {
 
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-        printOdd(numbers);
+        //printOdd(numbers);
         List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker", "Kubernetes");
         //fourLetterCourses(courses);
-        courses.stream().map(c -> c + " " + c.length()).forEach(System.out::println);
+       // courses.stream().map(c -> c + " " + c.length()).forEach(System.out::println);
+       // courses.stream().sorted(Comparator.comparing(s->s.length())).forEach(System.out::println);
+
+        List<Integer> evenList=numbers.stream().filter(s->s%2==0).sorted().distinct().collect(Collectors.toList());
+        System.out.println(evenList);
 
     }
 
