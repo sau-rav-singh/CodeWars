3187280f1fbf5da6f1a4b811efd83670f2cde16e
Saurav Singh <singh.saurav@icloud.com>
2023-12-16 18:46:31 +0530

    Added J8 Files

---
+++ src/test/java8/data/Bike.java
@@ -0,0 +1,22 @@
+package data;
+
+import lombok.Data;
+
+@Data
+public class Bike {
+    private String name;
+    private String model;
+
+    public Bike(String name, String model) {
+        this.name = name;
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Bike{" +
+                "name='" + name + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java8/defaults/Multiplier.java
@@ -0,0 +1,21 @@
+package defaults;
+
+import java.util.List;
+
+/**
+ * Created by z001qgd on 7/31/18.
+ */
+public interface Multiplier {
+
+    int multiply(List<Integer> integerList);
+
+    default int size(List<Integer> integerList){
+        System.out.println("Inside interface");
+        return integerList.size();
+    }
+
+    static boolean isEmpty(List<Integer> integerList){
+
+        return integerList!=null && integerList.size()==0;
+    }
+}
