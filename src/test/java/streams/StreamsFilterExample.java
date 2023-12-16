6cfc284733502132647ad45af0027d5ba2e1abe0
Saurav Singh <singh.saurav@icloud.com>
2023-12-16 19:20:50 +0530

    Added J8 Files

--- src/test/java/streams/StreamsFilterExample.java
+++ src/test/java/streams/StreamsFilterExample.java
@@ -2,23 +2,18 @@ package streams;
 
 import data.Student;
 import data.StudentDataBase;
-
 import java.util.List;
-import java.util.stream.IntStream;
-
 import static java.util.stream.Collectors.toList;
 
 public class StreamsFilterExample {
 
     public static List<Student> filterStudents(){
 
-        List<Student> filteredStudentList = StudentDataBase.getAllStudents()
+        return StudentDataBase.getAllStudents()
                 .stream()
                 .filter(student -> student.getGpa()>=3.9)
                 .filter(student -> student.getGender().equals("female"))
                 .collect(toList());
-
-        return filteredStudentList;
     }
 
     public static void main(String[] args) {
