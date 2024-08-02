ba48c64d47cc2dd7d9a4be845196506985c3f1a3
Saurav Singh <singh.saurav@icloud.com>
2024-08-02 07:44:09 +0530

    added interviewBit

--- src/test/java/InterviewBit/BinarySearch.java
+++ src/test/java/InterviewBit/BinarySearch.java
@@ -6,14 +6,14 @@ public class BinarySearch {
         System.out.println(solution(new int[]{1, 3, 4, 4, 6},4));
     }
 
-    public static int solution(int[] A, int B) {
+    public static int solution(int[] arr, int num) {
         int left = 0;
-        int right = A.length - 1;
+        int right = arr.length - 1;
 
         while (left <= right) {
             int mid = (right + left) / 2;
 
-            if (A[mid] <= B) {
+            if (arr[mid] <= num) {
                 left = mid + 1;  // move right to find the last occurrence
             } else {
                 right = mid - 1; // move left
